13d6a29cca10d3a74d1fd8c87504477c6077b6be
Teja Nerella <nerella2@illinois.edu>
2025-11-04 19:24:55 -0600

    checking users endpoint

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,15 @@
+const User = require('../models/user.js');
+
 module.exports = function (router) {
 
     var userRoute = router.route('/');
+    var userIdRoute = router.route('/:id');
 
 
-    userRoute.get(function (req, res) {
+    userRoute.get(async function (req, res) {
         var connectionString = process.env.TOKEN;
-        res.json({ message: 'My connection string is ' + connectionString });
+        const user=await User.find();
+        res.json({ message: 'My connection string is ' + connectionString, data:user});
     });
 
     return router;
